0d31d8f045c57710d45da06ad54a77069c5b3081
franck <carnet.franck.paul@gmail.com>
2023-06-06 10:03:53 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -14,6 +14,6 @@ use Dotclear\Helper\L10n;
 L10n::$locales['TwentyTwenty'] = 'TwentyTwenty';
 L10n::$locales['TwentyTwenty effect for before/after images'] = 'Effet TwentyTwenty pour les images avant/après';
 L10n::$locales['Enable TwentyTwenty'] = 'Activer TwentyTwenty';
-L10n::$locales['Note that this feature requires jQuery 3+.'] = '';
+L10n::$locales['Note that this feature requires jQuery 3+.'] = 'Notez que cette fonctionnalité nécessite la version 3 ou supérieure de jQuery.';
 L10n::$locales['Before'] = 'Avant';
 L10n::$locales['After'] = 'Après';
