de9a65106c52732eefe120ecd2967150e1e13930
franck <carnet.franck.paul@gmail.com>
2023-10-24 13:22:36 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'TwentyTwenty',
     'TwentyTwenty effect for before/after images',
     'Franck Paul and contributors',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
